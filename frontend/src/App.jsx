b3ff1d1643367a902edbdbc1f897c5719bcff9e0
Arslan Iftikhar <arslaniftikhar533@gmail.com>
2026-01-21 12:37:15 +0500

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tasks, setTasks] = useState([]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Tasks List</h1>
+      <div class="add-form">
+        <input type="text" id="input" placeholder="Add a new task" />
+        <button id="add">Add Task</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <ul id="todos"></ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
