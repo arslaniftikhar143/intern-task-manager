674f36442b5e69bc2f50d38f6421e11ef1df688a
Arslan Iftikhar <arslaniftikhar533@gmail.com>
2026-01-21 11:07:23 +0500

    changes

--- models/task.models.js
+++ models/task.models.js
@@ -12,11 +12,7 @@ const taskSchema = new Schema(
       required: true,
     },
     status: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: String,
+      type: Boolean,
       required: true,
     },
   },
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,24 +1,36 @@
 const router = require("express").Router();
 const Task = require("../models/task.models.js");
 
+function validateString(title) {
+  const regex = /^[A-Za-z][A-Za-z0-9 ]*$/;
+  return regex.test(title);
+}
+
 router.get("/", async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json(tasks);
 });
 
 router.post("/", async (req, res) => {
-  const { title, description, status, createdAt } = req.body;
+  const { title, description, status } = req.body;
 
-  if (title || description || status || createdAt) {
-    res.status(400).send("All fields are required");
-    return;
+  console.log(req.body);
+
+  //   if (!title || !description || !status) {
+  //     return res.status(400).send("All fields are required");
+  //   }
+
+  if (!validateString(title)) {
+    return res.status(400).send("Please enter valid title");
+  }
+  if (!validateString(description)) {
+    return res.status(400).send("Please enter valid description");
   }
 
   const createTask = await Task.create({
     title,
     description,
     status,
-    createdAt,
   });
 
   res.status(200).json({
