08a508176fefce5cc98573077e58795096b0494d
Arslan Iftikhar <arslaniftikhar533@gmail.com>
2026-01-22 13:27:51 +0500

    added morgan and created delete & put api endpoints

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,11 +1,17 @@
 const {
   handleGetTasks,
   handleCreateTask,
+  handleEditTask,
+  handleDeleteTask,
+  handleGetTask,
 } = require("../controllers/task.controller");
 
 const router = require("express").Router();
 
 router.get("/", handleGetTasks);
 router.post("/", handleCreateTask);
+router.put("/:id", handleEditTask);
+router.delete("/:id", handleDeleteTask);
+router.get("/:id", handleGetTask);
 
 module.exports = router;
