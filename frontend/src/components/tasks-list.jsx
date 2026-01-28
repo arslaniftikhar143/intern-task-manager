d19f17adb70eece3ff90189e68092c3a701ae2ed
Arslan Iftikhar <arslaniftikhar533@gmail.com>
2026-01-28 15:55:11 +0500

    updated filters

--- frontend/src/components/tasks-list.jsx
+++ frontend/src/components/tasks-list.jsx
@@ -6,54 +6,68 @@ export function TasksList({
 }) {
   return (
     <ul id="todos">
-      {data.map((task) => (
-        <div key={task._id}>
-          <li>
-            {task.title} :{" "}
-            <span
-              style={{
-                fontSize: "10px",
+      {data.length === 0 ? (
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          No tasks found
+        </div>
+      ) : (
+        data.map((task) => (
+          <div key={task._id}>
+            <li>
+              {task.title} :{" "}
+              <span
+                style={{
+                  fontSize: "10px",
+                }}
+              >
+                {task.description}
+              </span>
+            </li>
+
+            <button
+              type="button"
+              onClick={() => {
+                handleToggleTask(task._id);
+              }}
+              id="toggle-button"
+              className={task?.status ? "completed" : ""}
+            >
+              {task.status ? "Mark Not Done" : "Mark Done"}
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setEditTask({
+                  id: task._id,
+                  title: task.title,
+                  description: task.description,
+                  status: task.status,
+                });
+              }}
+              id="edit-button"
+            >
+              Edit
+            </button>
+            <button
+              type="button"
+              id="delete-button"
+              onClick={() => {
+                handleDeleteTask(task._id);
               }}
             >
-              {task.description}
-            </span>
-          </li>
-
-          <button
-            type="button"
-            onClick={() => {
-              handleToggleTask(task._id);
-            }}
-            id="toggle-button"
-            className={task?.status ? "completed" : ""}
-          >
-            {task.status ? "Mark Not Done" : "Mark Done"}
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              setEditTask({
-                id: task._id,
-                title: task.title,
-                description: task.description,
-                status: task.status,
-              });
-            }}
-            id="edit-button"
-          >
-            Edit
-          </button>
-          <button
-            type="button"
-            id="delete-button"
-            onClick={() => {
-              handleDeleteTask(task._id);
-            }}
-          >
-            Delete Task
-          </button>
-        </div>
-      ))}
+              Delete Task
+            </button>
+          </div>
+        ))
+      )}
     </ul>
   );
 }
